aec1b073fb6aaaa3a0b046dc7277f28552fd1b97
freemt <yucongo+fmt@gmail.com>
2022-07-28 13:21:16 +0800

    Update config.js themeConfig try

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,10 +1,11 @@
 export default {
   title: 'mu\'s github.io page',
   description: 'Just playing around...',
-  lastUpdated: true
+  lastUpdated: true,
   themeConfig: {
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2022 mu'
     }
+  }
 }
